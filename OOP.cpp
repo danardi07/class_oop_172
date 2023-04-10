deadc2c8f77a5279a2118e12ad5f40f168dcb933
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2023-04-10 15:44:52 +0700

    class mahasiswa

--- OOP.cpp
+++ OOP.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 using namespace std;
 
@@ -9,17 +8,49 @@ public:
 	void tampil() {
 		cout << "NIM= " << nim;
 		cout << "NAMA= " << nama;
-		
+
 	}
-	
+};
+
 	class MataKuliah {
 	private:
 		int sks;
 		string kode;
 		string namaMK;
 	public:
+		void inputMK() {
+			cout << "\nMasukan Jumlah SKS= ";
+			cin >> sks;
+			cout << "\nMasukan Kode MK= ";
+			cin >> kode;
+			cout << "\nMasukan Nama MK= ";
+			cin >> namaMK;
+		}
+
+
+
+		void tampilMK() {
+			cout << "\Jumlah SKS= " << sks;
+			cout << "\nKode MK= " << kode;
+			cout << "\nNama MK= " << namaMK;
+
+		};
+	};
+
+	int main() {
+
+		Mahasiswa mhs1;
+		MataKuliah mk;
+
+		cout << "Masukan NIM = ";
+		cin >> mhs1.nim;
+		cout << "Masukan Nama = ";
+		cin >> mhs1.nama;
+
+		mk.inputMK();
+		mhs1.tampil();
+		mk.tampilMK();
 
 	};
-};
 
-int main()
+
